b907ee965d6913ed6a33a67e2374aa97fca63a0e
Steven Samson <kosai03@gmail.com>
2016-11-12 12:57:40 -0800

    Updated dependencies and arg checks

--- bin/textToSequence.sh
+++ bin/textToSequence.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [ $# -ne 2 ]; then
+    echo "usage: textToSequence.sh TEXT_FILE_DIR OUTPUT_SEQUENCE"
+    exit 1
+fi
+
 scala -cp target/minhash-document-clusters-1.0-SNAPSHOT.jar com.stevens.transform.TextToSequence $1 $2
 
 exit 0
