dd55b6ef0109a7293811f7d972d71fbe873b1c0f
Steven Samson <steven.a.samson@gmail.com>
2016-12-18 20:21:44 -0800

    Updated hash format to int

--- src/main/scala/com/stevens/minhash/MinHashDocument.scala
+++ src/main/scala/com/stevens/minhash/MinHashDocument.scala
@@ -7,16 +7,16 @@ import com.google.common.hash._
 class MinHashDocument(text: String, signatureLength: Int = 100, shingleLength: Int = 5, seed: Int = 5) {
   private val seededRandom: Random = new Random(seed)
   lazy val shingles: Set[String] = createShingles().toSet
-  lazy val signature: Set[String] = createMinhashSignature()
+  lazy val signature: Set[Int] = createMinhashSignature()
 
   private def createShingles(): Iterator[String] = {
     text.split("\\s+").sliding(shingleLength).map(gram => gram.mkString(" "))
   }
 
-  private def createMinhashSignature(): Set[String] = {
+  private def createMinhashSignature(): Set[Int] = {
     generateRandomSeeds().map { randomSeed => 
       val hashFunction: HashFunction = Hashing.murmur3_32(randomSeed)
-      createShingles().map(shingle => hashFunction.hashString(shingle, StandardCharsets.UTF_8).toString).min
+      createShingles().map(shingle => hashFunction.hashString(shingle, StandardCharsets.UTF_8).asInt).min
     }.toSet
   }
 
@@ -38,7 +38,7 @@ object MinHashDocument {
     shingles1.intersect(shingles2).size.toDouble / shingles1.union(shingles2).size.toDouble
   }
   
-  def minHashSimilarity(hash1: Set[String], hash2: Set[String]): Double = {
+  def minHashSimilarity(hash1: Set[Int], hash2: Set[Int]): Double = {
     hash1.intersect(hash2).size.toDouble / hash1.union(hash2).size.toDouble
   }
 }
